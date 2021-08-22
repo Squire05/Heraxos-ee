1f3807b5ab3123e1e39d017d0050a6eb119f0b98
Suro299 <88720217+Suro299@users.noreply.github.com>
2021-08-22 19:51:02 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // JS FILE
+
+let time_div = document.getElementById("time_div");
+
+setInterval(() => {
+    let date = new Date()
+    let h = date.getHours();
+    let m = date.getMinutes()
+    time_div.innerText = h + ":" + m 
+}, 1000);
