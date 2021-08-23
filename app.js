bdab97cc49e79f96d756c741a9ff51ae68bf8188
Suro299 <88720217+Suro299@users.noreply.github.com>
2021-08-23 19:27:59 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -8,14 +8,20 @@ let time_div_header = document.getElementById("time_div_header");
 
 
 setInterval(() => {
-    let date = new Date()
+    let date = new Date();
     let h = date.getHours();
     let m = date.getMinutes();
-    let s = date.getSeconds()
+    let s = date.getSeconds();
+
+    if (m <= 9) {
+        m = "0" + m
+    }
 
     time_div_block_screen.innerText = h + ":" + m;
 
     time_div_header.innerText = h + ":" + m;
+
+
 
     
 }, 1000);
