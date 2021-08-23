264a88bdae6a833c56a1fe88bd72434cb1be976e
Suro299 <88720217+Suro299@users.noreply.github.com>
2021-08-23 18:58:33 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 // JS FILE
 
-let time_div = document.getElementById("time_div");
+const block_screen = document.getElementById("block_screen");
+const home_button = document.getElementById("home_button");
+
+let time_div_block_screen = document.getElementById("time_div_block_screen");
+let time_div_header = document.getElementById("time_div_header");
+
 
 setInterval(() => {
     let date = new Date()
     let h = date.getHours();
-    let m = date.getMinutes()
-    time_div.innerText = h + ":" + m 
+    let m = date.getMinutes();
+    let s = date.getSeconds()
+
+    time_div_block_screen.innerText = h + ":" + m;
+
+    time_div_header.innerText = h + ":" + m;
+
+    
 }, 1000);
+
+
+home_button.addEventListener("click", () => {
+    block_screen.style.transform = "translateY(-150%)"
+}) 
