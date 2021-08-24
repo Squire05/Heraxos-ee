afcf03c82207f2100ee7b0c642c558e1c3195176
Suro299 <88720217+Suro299@users.noreply.github.com>
2021-08-24 18:27:34 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-// JS FILE
 
+// Screens
 const block_screen = document.getElementById("block_screen");
+const settings_app_screen = document.getElementById("settings_app_screen")
+
+
+//Buttons
 const home_button = document.getElementById("home_button");
+
+
+// apps
+const settings_app = document.getElementById("settings_app")
+
+
+// TUMB
+const airplane_mod_tumb = document.getElementById("airplane_mod_tumb");
+const airplane_mod_tumb_cyc = document.getElementById("airplane_mod_tumb_cyc");
+
+// 0-OFF 1-ON
+let tumb_of_on = 0
+
 
 let time_div_block_screen = document.getElementById("time_div_block_screen");
 let time_div_header = document.getElementById("time_div_header");
+
+let scr_block_or = 1;
 
 
 setInterval(() => {
@@ -28,5 +47,33 @@ setInterval(() => {
 
 
 home_button.addEventListener("click", () => {
-    block_screen.style.transform = "translateY(-150%)"
+    if (scr_block_or == 1) {
+        block_screen.style.transform = "translateY(-150%)"
+    }
+
+    settings_app_screen.style.display = "none"
 }) 
+
+settings_app.addEventListener("click", () => {
+    settings_app_screen.style.display =  "flex"
+})
+
+
+//TUMB OFF-ON
+
+airplane_mod_tumb.addEventListener("click", () =>  {
+    if (tumb_of_on == 0) {
+
+        airplane_mod_tumb.style.backgroundColor = "green";
+        airplane_mod_tumb_cyc.style.marginLeft = "47%";
+
+        tumb_of_on = 1
+
+    } else if (tumb_of_on == 1) {
+
+        airplane_mod_tumb.style.backgroundColor = "#404040";
+        airplane_mod_tumb_cyc.style.marginLeft = "-4%";
+
+        tumb_of_on  = 0
+    }
+})
